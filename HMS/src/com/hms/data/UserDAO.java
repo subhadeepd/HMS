ad0403dc2fa71a0fe445773da300a2f62cc4a33c
sonuahluwalia <sonu.ahluwalia.2000@gmail.com>
2015-05-20 20:19:20 +0530

    added getter and setters

--- HMS/src/com/hms/data/UserDAO.java
+++ HMS/src/com/hms/data/UserDAO.java
@@ -4,5 +4,29 @@ package com.hms.data;
 //this class will create a userdao object with the information coming from the web
 //and insert the userdao in the user table
 public class UserDAO {
+	
+	private String name = "Joe";
+	private int age = 32;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	//this method will insert into database
+	public void insert() {
+
+		
+	}
+	
+	
 
 }
