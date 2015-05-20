7eec7e7d201a32907008d312a183873d967182da
sonuahluwalia <sonu.ahluwalia.2000@gmail.com>
2015-05-20 20:06:37 +0530

    added comment for the classes to define their purpose

--- HMS/src/com/hms/data/DatabaseConnection.java
+++ HMS/src/com/hms/data/DatabaseConnection.java
@@ -1,5 +1,6 @@
 package com.hms.data;
 
+//this is the class which will be used to connect to the database
 public class DatabaseConnection {
 
 }
--- HMS/src/com/hms/data/UserDAO.java
+++ HMS/src/com/hms/data/UserDAO.java
@@ -1,5 +1,8 @@
 package com.hms.data;
 
+
+//this class will create a userdao object with the information coming from the web
+//and insert the userdao in the user table
 public class UserDAO {
 
 }
